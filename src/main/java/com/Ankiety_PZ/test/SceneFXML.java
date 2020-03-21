77eb72cfb5e4ccca873422ba60219f72103e85aa
KamDziok <kam_dziok@wp.pl>
2020-03-21 15:46:20 +0100

    Stworzenie klasy przetrzymującej linki do plików fxml w postaci zmiennych.

---
+++ src/main/java/com/Ankiety_PZ/test/SceneFXML.java
@@ -0,0 +1,16 @@
+package com.Ankiety_PZ.test;
+
+/**
+ * Klasa przechowująca linki do plików fxml, zawierających sceny.
+ *
+ * @author KamDziok
+ */
+
+public class SceneFXML {
+
+    /**
+     * Odnośnik do poliku z panele logowania
+     */
+    public static final String PANEL_LOGIN = "PanelLogin";
+
+}
--- src/main/java/com/Ankiety_PZ/test/TestFX.java
+++ src/main/java/com/Ankiety_PZ/test/TestFX.java
@@ -20,7 +20,7 @@ public class TestFX extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("PanelLogin"));
+        scene = new Scene(loadFXML(SceneFXML.PANEL_LOGIN));
         stage.setScene(scene);
         stage.show();
     }
