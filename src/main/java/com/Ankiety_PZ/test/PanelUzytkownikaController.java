fdd4868482064d6fb08f43aa2f33d4fbf642af19
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-03-21 12:29:13 +0100

    stworzenie plików

---
+++ src/main/java/com/Ankiety_PZ/test/PanelUzytkownikaController.java
@@ -0,0 +1,16 @@
+package com.Ankiety_PZ.test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PanelUzytkownikaController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
