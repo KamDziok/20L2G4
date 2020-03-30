2531628dff096c5e5d26028b3d3778478e1bc501
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-03-30 17:43:55 +0200

    poprawienie panelu uzytkownika i dodanie okna ankiety

---
+++ src/main/java/com/Ankiety_PZ/test/OknoAnkietyController.java
@@ -0,0 +1,16 @@
+package com.Ankiety_PZ.test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OknoAnkietyController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
--- src/main/java/com/Ankiety_PZ/test/PanelUzytkownikaController.java
+++ src/main/java/com/Ankiety_PZ/test/PanelUzytkownikaController.java
@@ -1,7 +1,12 @@
 package com.Ankiety_PZ.test;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PanelUzytkownikaController extends Application {
 
@@ -10,7 +15,10 @@ public class PanelUzytkownikaController extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+//        Parent root = FXMLLoader.load(getClass().getResource("panelUzytkownika.fxml"));
+//        primaryStage.setTitle("Hello");
+//        primaryStage.setScene(new Scene(root, 300, 300));
+//        primaryStage.show();
     }
 }
