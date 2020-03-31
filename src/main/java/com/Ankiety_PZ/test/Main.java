2739fa29c984be009fa3c2b05ffe4fcb2e99668b
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-03-31 12:51:54 +0200

    kosmetyczne zmiany w oknie wypełniania ankiety

--- src/main/java/com/Ankiety_PZ/test/Main.java
+++ src/main/java/com/Ankiety_PZ/test/Main.java
@@ -1,9 +1,14 @@
 package com.Ankiety_PZ.test;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Main {
 
     public static void main(String[] args) {
         TestFX.main(args);
     }
-
 }
--- src/main/java/com/Ankiety_PZ/test/TestFX.java
+++ src/main/java/com/Ankiety_PZ/test/TestFX.java
@@ -2,13 +2,20 @@ package com.Ankiety_PZ.test;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
 
 public class TestFX extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(new Pane(), 800, 600));
+        Parent root = FXMLLoader.load(getClass().getResource("panelUzytkownika.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Hello");
+        stage.setScene(scene);
         stage.show();
     }
 
