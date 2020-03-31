417c90203910ff6c32ddd1553027b6453572c244
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-03-31 16:07:54 +0200

    dodanie obslogi przycisku zakoncz

--- src/main/java/com/Ankiety_PZ/test/OknoAnkietyController.java
+++ src/main/java/com/Ankiety_PZ/test/OknoAnkietyController.java
@@ -1,16 +1,29 @@
+/**
+ * Sample Skeleton for 'oknoAnkiety.fxml' Controller Class
+ */
+
 package com.Ankiety_PZ.test;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
-public class OknoAnkietyController extends Application {
+public class OknoAnkietyController extends BulidStage{
 
-    public static void main(String[] args) {
-        launch(args);
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML
+    void oknoAnkietyButtonFinish(ActionEvent event) {
+        deleteStage(event);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
 
     }
 }
