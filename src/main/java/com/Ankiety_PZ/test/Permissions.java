0939bc76a058c605981e35cd907aa71ace81dbf4
KamDziok <kam_dziok@wp.pl>
2020-04-27 13:11:30 +0200

    PZ-50 #comment Wprowadzanie klasy Permission z kodowaniem uprawnień.

---
+++ src/main/java/com/Ankiety_PZ/test/Permissions.java
@@ -0,0 +1,10 @@
+package com.Ankiety_PZ.test;
+
+public class Permissions {
+
+    public static final int ADMIN = 0;
+    public static final int ANKIETER = 1;
+    public static final int OSOBA_OD_NAGROD = 2;
+    public static final int KLIENT = 3;
+
+}
