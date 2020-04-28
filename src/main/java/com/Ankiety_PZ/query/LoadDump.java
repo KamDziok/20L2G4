edc2654881c174b07b4ddad25426a375e292fd32
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-04-28 11:48:32 +0200

    PZ-47 coment# aktualizacja kodu

--- src/main/java/com/Ankiety_PZ/query/LoadDump.java
+++ src/main/java/com/Ankiety_PZ/query/LoadDump.java
@@ -1,24 +1,29 @@
 package com.Ankiety_PZ.query;
 
-import org.hibernate.*;
-
 import java.io.File;
 import java.util.Scanner;
 
 public class LoadDump extends OperationInSession {
     public void loadDump(String sciezka) throws Exception {
         try {
-            String komendy = null;
+            String komendy = "";
+            String odczyt;
             Scanner plik = new Scanner(new File(sciezka));
 
-            while (plik.hasNextLine()) {
-                komendy += plik.nextLine();
-            }
+            System.out.println(komendy);
 
             session = openSession();
             transaction = beginTransaction(session);
-            query = session.createNativeQuery("BEGIN " + komendy + " END;");
-            query.executeUpdate();
+            while (plik.hasNextLine()) {
+                odczyt = plik.nextLine();
+                if(!odczyt.startsWith("--") && !odczyt.startsWith("/*")) komendy += odczyt;
+                if(odczyt.endsWith(";") && !odczyt.endsWith("*/;")) {
+                    System.out.println(komendy);
+                    query = session.createNativeQuery(komendy);
+                    query.executeUpdate();
+                    komendy = "";
+                }
+            }
         } catch(Exception e){
             logException(e);
         }finally{
