678299da902952f43fc5b4250e951cc2feee3ad0
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-04-28 15:21:12 +0200

    PZ-51 coment# dodanie interfejsu SetStartValues i dodanie funkcjonalnosci wyswietlania danych uzytkownika w ustawieniach

--- src/main/java/com/Ankiety_PZ/test/PanelUzytkownikaController.java
+++ src/main/java/com/Ankiety_PZ/test/PanelUzytkownikaController.java
@@ -4,23 +4,47 @@
 
 package com.Ankiety_PZ.test;
 
+import com.Ankiety_PZ.biznes.Klient;
+import com.Ankiety_PZ.biznes.Uzytkownik;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class PanelUzytkownikaController extends BulidStage{
+public class PanelUzytkownikaController extends BulidStage implements SetStartValues{
 
+    private Uzytkownik curentUser;
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
-
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
-
     @FXML
     private Button wyloguj;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField haslo;
+    @FXML
+    private TextField nowehaslo;
+    @FXML
+    private TextField hasloznowu;
+    @FXML
+    private Button imie;
+    @FXML
+    private TextField nazwisko;
+    @FXML
+    private TextField miejscowosc;
+    @FXML
+    private TextField ulica;
+    @FXML
+    private TextField budynek;
+    @FXML
+    private TextField lokal;
+    @FXML
+    private TextField kod;
 
     @FXML
     void wyloguj(ActionEvent event) {
@@ -36,5 +60,18 @@ public class PanelUzytkownikaController extends BulidStage{
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
             assert wyloguj != null : "fx:id=\"wyloguj\" was not injected: check your FXML file 'panelUzytkownika.fxml'.";
+    }
+
+    @Override
+    public void setStartValues(Uzytkownik user, Klient customer) {
+        curentUser = user;
+        email.setText(user.getMail());
+        imie.setText(user.getImie());
+        nazwisko.setText(user.getNazwisko());
+        miejscowosc.setText(customer.getMiejscowosc());
+        ulica.setText(customer.getUlica());
+        budynek.setText(customer.getNumerBloku());
+        lokal.setText(customer.getNumerLokalu());
+        kod.setText(customer.getKodPocztowy());
     }
 }
---
+++ src/main/java/com/Ankiety_PZ/test/SetStartValues.java
@@ -0,0 +1,11 @@
+package com.Ankiety_PZ.test;
+
+import com.Ankiety_PZ.biznes.Klient;
+import com.Ankiety_PZ.biznes.Uzytkownik;
+
+public interface SetStartValues {
+
+    default void setStartValues(Uzytkownik user, Klient customer) {
+
+    }
+}
