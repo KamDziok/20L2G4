eecf220add5ec4268b064aa66fa4baf434956d9c
KamDziok <kam_dziok@wp.pl>
2020-05-01 12:10:49 +0200

    PZ-54 #comment Dodanie funkcji do ładowania wszystkich ankiet i ładowani aktywnych ankiet.

---
+++ src/main/java/com/Ankiety_PZ/query/AnkietyQuery.java
@@ -0,0 +1,59 @@
+package com.Ankiety_PZ.query;
+
+import com.Ankiety_PZ.hibernate.Ankiety;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Klasa zawiera metody do przesyłu danych z bazą danych dla tabeli Ankiety.
+ */
+
+public class AnkietyQuery extends OperationInSession {
+
+    /**
+     * Metoda przesyła listę wszystkich Ankiet.
+     *
+     * @return lista wszystkich Ankiet, jeśli rozmiar listy wynosi 0 znaczy to, że nie ma ankiet w bazie.
+     */
+
+    public List<Ankiety> selectAll() {
+        List<Ankiety> ankiety = new ArrayList<>();
+        try {
+            session = openSession();
+            criteria = session.createCriteria(Ankiety.class);
+            ankiety = criteria.list();
+        } catch(Exception e){
+            logException(e);
+        }finally{
+            sessionClose(session);
+        }
+        return ankiety;
+    }
+
+    /**
+     * Metoda przesyła listę Ankiet aktywnych.
+     *
+     * @return lista aktywnych Ankiet, jeśli rozmiar listy wynosi 0 znaczy, że nie ma dostępnych ankiet.
+     */
+    public List<Ankiety> selectAllActiveAnkiety(){
+        List<Ankiety> ankiety = new ArrayList<>();
+        try{
+            session = openSession();
+            Date date = new Date();
+            ankiety = session
+                    .createQuery("from Ankiety AS a where :date between a.dataRozpoczecia and a.dataZakonczenia")
+                    .setParameter("date", date)
+                    .list();
+        }catch(Exception e){
+            logException(e);
+        }finally {
+            sessionClose(session);
+        }
+        return ankiety;
+    }
+
+
+
+}
