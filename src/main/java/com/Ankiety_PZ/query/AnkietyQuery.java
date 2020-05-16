ff2f14abcb8aa98d258b2b7a18ab7a00c8fbe280
KamDziok <kam_dziok@wp.pl>
2020-05-16 15:57:53 +0200

    PZ-67 #comment Poprawa błędnego warunku.

--- src/main/java/com/Ankiety_PZ/query/AnkietyQuery.java
+++ src/main/java/com/Ankiety_PZ/query/AnkietyQuery.java
@@ -112,7 +112,7 @@ public class AnkietyQuery extends OperationInSession {
                 }
                 ankiety.getPytanias().forEach(pytaniaObj -> {
                     Pytania pytania = (Pytania) pytaniaObj;
-                    if(pytania.getIdPytania() != null){
+                    if(!pytania.getIdPytania().equals(null)){
                         Pytania oldPytania = pytaniaQuery.selectByID(pytania.getIdPytania());
                         if(!pytania.isTheSame(oldPytania)) {
                             pytaniaQuery.updatePytaniaWithOutTransaction(pytania, session);
@@ -122,7 +122,7 @@ public class AnkietyQuery extends OperationInSession {
                     }
                     pytania.getOdpowiedzis().forEach(odpowiedziObj -> {
                         Odpowiedzi odpowiedzi = (Odpowiedzi) odpowiedziObj;
-                        if(odpowiedzi.getIdOdpowiedzi() != null){
+                        if(!odpowiedzi.getIdOdpowiedzi().equals(null)){
                             Odpowiedzi oldOdpowiedzi = odpowiedziQuery.selectByID(odpowiedzi.getIdOdpowiedzi());
                             if(odpowiedzi.isTheSame(oldOdpowiedzi)) {
                                 odpowiedziQuery.updateOdpowiedziWithOutTransaction(odpowiedzi, session);
