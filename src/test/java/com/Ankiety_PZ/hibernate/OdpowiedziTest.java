53e60ac6b1d94d179b47cd2cc584e48e66afdc6f
KamDziok <kam_dziok@wp.pl>
2020-06-01 13:13:39 +0200

    PZ-70 #comment Dodanie testów jednostkowych do klasy Odpowiedzi.

---
+++ src/test/java/com/Ankiety_PZ/hibernate/OdpowiedziTest.java
@@ -0,0 +1,60 @@
+package com.Ankiety_PZ.hibernate;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OdpowiedziTest {
+
+    private static Odpowiedzi odpowiedziA;
+    private static Odpowiedzi odpowiedziACopy;
+    private static Odpowiedzi odpowiedziB;
+
+    static void setOdpowiedziACopy(Odpowiedzi odpowiedz){
+        odpowiedziACopy = new Odpowiedzi();
+        odpowiedziACopy.setIdOdpowiedzi(odpowiedz.getIdOdpowiedzi());
+        odpowiedziACopy.setOdpowiedz(odpowiedz.getOdpowiedz());
+    }
+
+    @BeforeAll
+    static void setValuesInObcejt(){
+        odpowiedziA = new Odpowiedzi();
+        odpowiedziA.setIdOdpowiedzi(1);
+        odpowiedziA.setOdpowiedz("odpowiedzA");
+        setOdpowiedziACopy(odpowiedziA);
+        odpowiedziB = new Odpowiedzi();
+        odpowiedziB.setIdOdpowiedzi(33);
+        odpowiedziB.setOdpowiedz("odpowiedzB");
+    }
+
+    @BeforeEach
+    void clearOdpowiedziACopy(){
+        setOdpowiedziACopy(odpowiedziA);
+    }
+
+    @Test
+    void isTheSameTrue(){
+        assertTrue(odpowiedziA.isTheSame(odpowiedziACopy));
+    }
+
+    @Test
+    void isTheSameDifferentAll(){
+        assertFalse(odpowiedziA.isTheSame(odpowiedziB));
+    }
+
+    @Test
+    void isTheSameDifferentId(){
+        odpowiedziACopy.setIdOdpowiedzi(23);
+        assertFalse(odpowiedziA.isTheSame(odpowiedziACopy));
+    }
+
+    @Test
+    void isTheSameDifferentOdpowiedz(){
+        odpowiedziACopy.setOdpowiedz("odpowiedzACopy");
+        assertFalse(odpowiedziA.isTheSame(odpowiedziACopy));
+    }
+
+}
