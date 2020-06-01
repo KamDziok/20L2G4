2a80a1fc60b77fb04c12783a068b9e2b57f81f81
banaskrzysztof <47889449+banaskrzysztof@users.noreply.github.com>
2020-06-01 11:53:04 +0200

    PZ-69 #comment Javadoc dla NagrodyGenerowaniePDF

--- src/main/java/com/Ankiety_PZ/hibernate/HibernateUtil.java
+++ src/main/java/com/Ankiety_PZ/hibernate/HibernateUtil.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.Ankiety_PZ.hibernate;
 
 import org.hibernate.SessionFactory;
@@ -14,6 +9,7 @@ import org.hibernate.cfg.Configuration;
  *
  * @author KamDziok
  */
+
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
