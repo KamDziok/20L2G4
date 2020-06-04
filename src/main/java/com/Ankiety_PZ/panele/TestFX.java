1702c2b6cfff19d9d41516ca90ad11022a435b91
KamDziok <kam_dziok@wp.pl>
2020-06-04 11:42:03 +0200

    Poprawa wychwycenia braku połączenia z bazą danych.

--- src/main/java/com/Ankiety_PZ/panele/TestFX.java
+++ src/main/java/com/Ankiety_PZ/panele/TestFX.java
@@ -5,8 +5,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 
 /**
@@ -35,7 +37,21 @@ public class TestFX extends Application {
         return fxmlLoader.load();
     }
 
+    public static Alert createDialogAlert(String massage){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Ankiety");
+        alert.setHeaderText(null);
+        alert.setContentText(massage);
+
+        alert.showAndWait();
+        return alert;
+    }
+
     public static void main(String[] args) {
-        launch();
+        if(ConnectToDataBase.connectToDataBase()){
+            launch();
+        }else {
+            JOptionPane.showMessageDialog(null, "Brak połączenia z bazą dancyh.");
+        }
     }
 }
