9410cc708373f115ed297721e89fb8a2b5557716
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-06-04 15:03:42 +0200

    baza danych

--- src/main/java/com/Ankiety_PZ/panele/TestFX.java
+++ src/main/java/com/Ankiety_PZ/panele/TestFX.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import javax.swing.*;
@@ -39,17 +38,25 @@ public class TestFX extends Application {
     }
 
     public static void main(String[] args) {
+        LoadDump dump = new LoadDump();
         if(ConnectToDataBase.connectToDataBase()){
-            launch();
+            try {
+                dump.loadDump("baza_danych/bazadanychtest/ankiety.sql");
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                launch();
+            }
         }else {
             JOptionPane.showMessageDialog(null, "Brak połączenia z bazą dancyh.");
+            try {
+                dump.loadDump("baza_danych/ankiety.sql");
+                dump.loadDump("baza_danych/bazadanychtest/ankiety.sql");
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                launch();
+            }
         }
-        LoadDump dump = new LoadDump();
-        try {
-            dump.loadDump("baza_danych/bazadanychtest/ankiety.sql");
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
-        launch();
     }
 }
